f6bb1faa9692b4a582ecc5432ee03450d14f6afb
IliasTaleb <ilias.taleb@ensea.fr>
2025-12-14 23:40:43 +0100

    Hopefully the last update of file qst1

--- question_1.c
+++ question_1.c
@@ -2,9 +2,9 @@
 
 
 void Welcome_prompt(){
-    write(STDOUT_FILENO, "Welcome to the shell !\n", size_of_welcome_prompt);
+    write(STDOUT_FILENO, Welcoming_message, size_of_welcome_prompt);
 }
 
 void Normal_prompt(){
-    write(STDOUT_FILENO, "enseaSH$\n", size_of_normal_prompt);
+    write(STDOUT_FILENO, Normal_message, size_of_normal_prompt);
 }
--- question_1.h
+++ question_1.h
@@ -2,7 +2,10 @@
 #include <unistd.h>
 
 #define size_of_welcome_prompt 23
-#define size_of_normal_prompt 7
+#define size_of_normal_prompt 8
+
+#define Welcoming_message "Welcome to the shell !\n"
+#define Normal_message "enseaSH$\n"
 
 void Welcome_prompt();
 void Normal_prompt();
