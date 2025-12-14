6ce94471bf06148ffecd29d695e9725361ed672d
IliasTaleb <ilias.taleb@ensea.fr>
2025-12-14 22:33:30 +0100

    Uploaded the main corresponding to the new qst1

--- main.c
+++ main.c
@@ -3,17 +3,9 @@
 int main() {
     Welcome_prompt();
     Normal_prompt();
-while(1){
-    char command_entered_by_user[128];  
-    Execution_of_a_simple_command(command_entered_by_user);
-    Normal_prompt();
-    if (feof(stdin)) {
-        Exit_prompt_or_ctrl_d_prompt();
-}
-
-
     return EXIT_SUCCESS;
 
 }
-}
+
+
 
